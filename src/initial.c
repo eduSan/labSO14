cf7adc03629215d63a4b61d80cb8053a0a054760
eduSan <eduardo.santarelli@studio.unibo.it>
2014-05-03 16:05:23 +0200

    Started implementing Kaya entry point (initial.c) 	new file:   src/initial.c

---
+++ src/initial.c
@@ -0,0 +1,15 @@
+#include <pcb.h>
+#include <asl.h>
+
+int main(void){
+
+	/**
+	 * The following code populates the ROM Reserved frame areas with
+	 * the data needed to handle various kind of interrupts
+	 * FIXME change comment once implementation is complete
+	 */
+
+
+
+}
+
